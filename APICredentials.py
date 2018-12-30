651efcf6c73a2e4016aacbc3f24e73786e5dcd0e
hesterdakota <hesterdakota@gmail.com>
2018-12-30 00:16:26 -0600

    Create APICredentials.py

---
+++ APICredentials.py
@@ -0,0 +1,7 @@
+# This is required by the twitter API in order to access tweets. See 
+# https://github.com/bear/python-twitter#api for more info
+
+consumer =          '' # Ensure that all variables are strings
+consumer_secret =   ''
+access =            ''
+access_secret =     ''
